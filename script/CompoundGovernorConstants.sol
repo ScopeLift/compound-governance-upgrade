b45dd679902f8a48e3554fe93a7d14309d081aad
John Feras <jferas@ferasinfotech.com>
2024-10-04 17:00:11 -0400

    forge fmt constant cleanup

--- script/CompoundGovernorConstants.sol
+++ script/CompoundGovernorConstants.sol
@@ -5,15 +5,12 @@ pragma solidity 0.8.26;
 contract CompoundGovernorConstants {
     // These constants are taken from the existing GovernorBravoDelegate contract.
 
-    uint48 INITIAL_VOTING_DELAY = 13_140; // The delay before voting on a proposal may take place, once proposed, in
-        // blocks
+    uint48 INITIAL_VOTING_DELAY = 13_140; // The delay before voting takes place, in blocks
     uint32 INITIAL_VOTING_PERIOD = 19_710; // The duration of voting on a proposal, in blocks
-    uint256 INITIAL_PROPOSAL_THRESHOLD = 25_000e18; // The number of votes required in order for a voter to become a
-        // proposer
+    uint256 INITIAL_PROPOSAL_THRESHOLD = 25_000e18; // Votes required in order for a voter to become proposer
     uint256 INITIAL_QUORUM = 400_000e18; // 400,000 = 4% of Comp
 
-    uint48 INITIAL_VOTE_EXTENSION = 7200; // About 2 days in blocks - Prevents sudden move/accumulation of tokens before
-        // voting.
+    uint48 INITIAL_VOTE_EXTENSION = 7200; // Prevents sudden token moves before voting ends (2 days of blocks)
 
     // The address of the COMP token
     address COMP_TOKEN_ADDRESS = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
