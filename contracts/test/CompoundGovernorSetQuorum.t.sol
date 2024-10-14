ca7038df31e57bf18c632027d4a1656d73eec62a
John Feras <jferas@ferasinfotech.com>
2024-10-14 11:12:23 -0400

    Added SetQuorum test via proposal test helper and refactored test files

---
+++ contracts/test/CompoundGovernorSetQuorum.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.26;
+
+import {ProposalTest} from "contracts/test/helpers/ProposalTest.sol";
+
+contract CompoundGovernorInitializeTest is ProposalTest {
+    function _submitPassAndExecuteProposalToSetNewQuorum(address _proposer, uint256 _amount) public {
+        address[] memory _targets = new address[](1);
+        _targets[0] = address(governor);
+
+        uint256[] memory _values = new uint256[](1);
+        _values[0] = 0;
+
+        bytes[] memory _calldatas = new bytes[](1);
+        _calldatas[0] = _buildProposalData("setQuorum(uint256)", abi.encode(_amount));
+
+        ConstructedProposal memory _proposal = ConstructedProposal(_targets, _values, _calldatas, "Set New Quorum");
+        _submitPassQueueAndExecuteProposal(_proposer, _proposal);
+    }
+
+    function _submitAndFailProposalToSetNewQuorum(address _proposer, uint256 _amount) public {
+        address[] memory _targets = new address[](1);
+        _targets[0] = address(governor);
+
+        uint256[] memory _values = new uint256[](1);
+        _values[0] = 0;
+
+        bytes[] memory _calldatas = new bytes[](1);
+        _calldatas[0] = _buildProposalData("setQuorum(uint256)", abi.encode(_amount));
+
+        ConstructedProposal memory _proposal = ConstructedProposal(_targets, _values, _calldatas, "Set New Quorum");
+        _submitAndFailProposal(_proposer, _proposal);
+    }
+
+    function testFuzz_SetQuorum(uint256 _newQuorum) public {
+        _newQuorum = uint224(bound(_newQuorum, 1, 1000));
+        _submitPassAndExecuteProposalToSetNewQuorum(delegatee, _newQuorum);
+        assertEq(governor.quorum(block.timestamp), _newQuorum);
+    }
+
+    function testFuzz_FailSetQuorum(uint256 _newQuorum) public {
+        vm.assume(_newQuorum != INITIAL_QUORUM);
+        _newQuorum = uint224(bound(_newQuorum, 1, 1000));
+        _submitAndFailProposalToSetNewQuorum(delegatee, _newQuorum);
+        assertNotEq(governor.quorum(block.timestamp), _newQuorum);
+    }
+}
