dcf87a76970c3ac8a40f79ca1258a28853ab8d2a
John Feras <jferas@ferasinfotech.com>
2024-10-30 12:16:23 -0400

    Cleaned up tests for proposalDeadline, proposalSnapshot, and proposalEta governor functions

--- contracts/test/CompoundGovernorEnumIds.t.sol
+++ contracts/test/CompoundGovernorEnumIds.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.26;
 
 import {ProposalTest} from "contracts/test/helpers/ProposalTest.sol";
 import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
+import {GovernorCountingSimpleUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
 import {console2} from "forge-std/Test.sol";
 
 contract CompoundGovernorEnumIdsTest is ProposalTest {
@@ -33,27 +34,35 @@ contract CompoundGovernorEnumIdsTest is ProposalTest {
 
     function testFuzz_ProposalDeadlineCorrectWithEnumeratedId(uint256 _newValue) public {
         _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        uint256 _clockAtSubmit = governor.clock();
         Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
         uint256 _proposalId = _submitProposal(delegatee, _proposal);
         uint256 _deadline = governor.proposalDeadline(_proposalId);
-        assertNotEq(_deadline, 0);
+        assertEq(_deadline, _clockAtSubmit + INITIAL_VOTING_DELAY + INITIAL_VOTING_PERIOD);
     }
 
     function testFuzz_ProposalSnapshotCorrectWithEnumeratedId(uint256 _newValue) public {
         _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        uint256 _clockAtSubmit = governor.clock();
         Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
         uint256 _proposalId = _submitProposal(delegatee, _proposal);
         uint256 _snapShot = governor.proposalSnapshot(_proposalId);
-        assertNotEq(_snapShot, 0);
+        assertEq(_snapShot, _clockAtSubmit + INITIAL_VOTING_DELAY);
     }
 
     function testFuzz_ProposalEtaCorrectWithEnumeratedId(uint256 _newValue) public {
         _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
         Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
         uint256 _proposalId = _submitProposal(delegatee, _proposal);
-        _passAndQueueProposal(_proposal, _proposalId);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        uint256 _timeOfQueue = block.timestamp;
+        governor.queue(
+            _proposal.targets, _proposal.values, _proposal.calldatas, keccak256(bytes(_proposal.description))
+        );
         uint256 _eta = governor.proposalEta(_proposalId);
-        assertNotEq(_eta, 0);
+        assertEq(_eta, _timeOfQueue + timelock.delay());
     }
 
     function testFuzz_ProposalProposerCorrectWithEnumeratedId(uint256 _newValue) public {
