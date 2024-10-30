0865b3a51e39ef2dc5e4cc5340f151703f57a435
John Feras <jferas@ferasinfotech.com>
2024-10-30 13:57:18 -0400

    Added more enumerated proposal ID related tests

--- contracts/test/CompoundGovernorEnumIds.t.sol
+++ contracts/test/CompoundGovernorEnumIds.t.sol
@@ -23,56 +23,6 @@ contract CompoundGovernorEnumIdsTest is ProposalTest {
         _proposal = Proposal(_targets, _values, _calldatas, _description);
     }
 
-    function testFuzz_ProposalIdsAreSequential(uint256 _newValue) public {
-        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
-        Proposal memory _proposal1 = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
-        uint256 _proposalId1 = _submitProposal(delegatee, _proposal1);
-        Proposal memory _proposal2 = _buildBasicProposal(_newValue + 1, "Second Proposal");
-        uint256 _proposalId2 = _submitProposal(delegatee, _proposal2);
-        assertEq(_proposalId2, _proposalId1 + 1);
-    }
-
-    function testFuzz_ProposalDeadlineCorrectWithEnumeratedId(uint256 _newValue) public {
-        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
-        uint256 _clockAtSubmit = governor.clock();
-        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
-        uint256 _proposalId = _submitProposal(delegatee, _proposal);
-        uint256 _deadline = governor.proposalDeadline(_proposalId);
-        assertEq(_deadline, _clockAtSubmit + INITIAL_VOTING_DELAY + INITIAL_VOTING_PERIOD);
-    }
-
-    function testFuzz_ProposalSnapshotCorrectWithEnumeratedId(uint256 _newValue) public {
-        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
-        uint256 _clockAtSubmit = governor.clock();
-        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
-        uint256 _proposalId = _submitProposal(delegatee, _proposal);
-        uint256 _snapShot = governor.proposalSnapshot(_proposalId);
-        assertEq(_snapShot, _clockAtSubmit + INITIAL_VOTING_DELAY);
-    }
-
-    function testFuzz_ProposalEtaCorrectWithEnumeratedId(uint256 _newValue) public {
-        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
-        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
-        uint256 _proposalId = _submitProposal(delegatee, _proposal);
-        vm.prank(delegatee);
-        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
-        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
-        uint256 _timeOfQueue = block.timestamp;
-        governor.queue(
-            _proposal.targets, _proposal.values, _proposal.calldatas, keccak256(bytes(_proposal.description))
-        );
-        uint256 _eta = governor.proposalEta(_proposalId);
-        assertEq(_eta, _timeOfQueue + timelock.delay());
-    }
-
-    function testFuzz_ProposalProposerCorrectWithEnumeratedId(uint256 _newValue) public {
-        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
-        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
-        uint256 _proposalId = _submitProposal(delegatee, _proposal);
-        address _proposer = governor.proposalProposer(_proposalId);
-        assertEq(_proposer, delegatee);
-    }
-
     function testFuzz_ProposalCreatedEventEmittedWithEnumeratedProposalId(uint256 _newValue) public {
         _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
         string memory _description = "Checking for enumearted proposal IDs on events";
@@ -95,5 +45,165 @@ contract CompoundGovernorEnumIdsTest is ProposalTest {
         uint256 _proposalId = _submitProposal(delegatee, _proposal);
         assertEq(_proposalId, _firstProposalId + 1);
         assertEq(governor.proposalCount(), _originalProposalCount + 1);
+    }
+
+    function testFuzz_ProposalIdsAreSequential(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal1 = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId1 = _submitProposal(delegatee, _proposal1);
+        Proposal memory _proposal2 = _buildBasicProposal(_newValue + 1, "Second Proposal");
+        uint256 _proposalId2 = _submitProposal(delegatee, _proposal2);
+        assertEq(_proposalId2, _proposalId1 + 1);
+    }
+
+    function testFuzz_ProposalDeadlineCorrectWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        uint256 _clockAtSubmit = governor.clock();
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        uint256 _deadline = governor.proposalDeadline(_proposalId);
+        assertEq(_deadline, _clockAtSubmit + INITIAL_VOTING_DELAY + INITIAL_VOTING_PERIOD);
+    }
+
+    function testFuzz_ZeroReturnedIf_ProposalDeadlineCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        uint256 _deadline = governor.proposalDeadline(_invalidProposalId);
+        assertEq(_deadline, 0);
+    }
+
+    function testFuzz_ProposalSnapshotCorrectWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        uint256 _clockAtSubmit = governor.clock();
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        uint256 _snapShot = governor.proposalSnapshot(_proposalId);
+        assertEq(_snapShot, _clockAtSubmit + INITIAL_VOTING_DELAY);
+    }
+
+    function testFuzz_ZeroReturnedIf_ProposalSnapshotCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        uint256 _snapShot = governor.proposalSnapshot(_invalidProposalId);
+        assertEq(_snapShot, 0);
+    }
+
+    function testFuzz_ProposalEtaCorrectWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        uint256 _timeOfQueue = block.timestamp;
+        governor.queue(_proposalId);
+        uint256 _eta = governor.proposalEta(_proposalId);
+        assertEq(_eta, _timeOfQueue + timelock.delay());
+    }
+
+    function testFuzz_ZeroReturnedIf_ProposalEtaCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        governor.queue(_proposalId);
+        uint256 _eta = governor.proposalEta(_invalidProposalId);
+        assertEq(_eta, 0);
+    }
+
+    function testFuzz_ProposalProposerCorrectWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        address _proposer = governor.proposalProposer(_proposalId);
+        assertEq(_proposer, delegatee);
+    }
+
+    function testFuzz_ZeroReturnedIf_ProposalProposerCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        address _proposer = governor.proposalProposer(_invalidProposalId);
+        assertEq(_proposer, address(0));
+    }
+
+    function testFuzz_ProposalQueuedCorrectlyWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        governor.queue(_proposalId);
+        assertEq(uint8(governor.state(_proposalId)), uint8(IGovernor.ProposalState.Queued));
+    }
+
+    function testFuzz_RevertIf_QueueCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        vm.expectRevert(abi.encodeWithSelector(IGovernor.GovernorNonexistentProposal.selector, _invalidProposalId));
+        governor.queue(_invalidProposalId);
+    }
+
+    function testFuzz_ProposalExecutedCorrectlyWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD + 1, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        governor.queue(_proposalId);
+        vm.warp(block.timestamp + timelock.delay() + 1);
+        governor.execute(_proposalId);
+        assertEq(uint8(governor.state(_proposalId)), uint8(IGovernor.ProposalState.Executed));
+        assertEq(governor.proposalThreshold(), _newValue);
+    }
+
+    function testFuzz_RevertIf_ExecutedWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD + 1, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        uint256 _proposalId = _submitProposal(delegatee, _proposal);
+        vm.assume(_invalidProposalId != _proposalId);
+        vm.prank(delegatee);
+        governor.castVote(_proposalId, uint8(GovernorCountingSimpleUpgradeable.VoteType.For));
+        vm.roll(vm.getBlockNumber() + INITIAL_VOTING_PERIOD + 1);
+        governor.queue(_proposalId);
+        vm.warp(block.timestamp + timelock.delay() + 1);
+        vm.expectRevert(abi.encodeWithSelector(IGovernor.GovernorNonexistentProposal.selector, _invalidProposalId));
+        governor.execute(_invalidProposalId);
+    }
+
+    function testFuzz_ProposalCanBeCanceledWithEnumeratedId(uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        vm.startPrank(delegatee);
+        uint256 _proposalId = governor.propose(_proposal.targets, _proposal.values, _proposal.calldatas, _proposal.description);
+        governor.cancel(_proposalId);
+        vm.stopPrank();
+        assertEq(uint8(governor.state(_proposalId)), uint8(IGovernor.ProposalState.Canceled));
+    }
+
+    function testFuzz_RevertIf_CancelCalledWithInvalidProposalId(uint256 _invalidProposalId, uint256 _newValue) public {
+        _newValue = bound(_newValue, INITIAL_PROPOSAL_THRESHOLD, INITIAL_PROPOSAL_THRESHOLD + 10);
+        Proposal memory _proposal = _buildBasicProposal(_newValue, "Set New Proposal Threshold");
+        vm.startPrank(delegatee);
+        uint256 _proposalId = governor.propose(_proposal.targets, _proposal.values, _proposal.calldatas, _proposal.description);
+        vm.assume(_invalidProposalId != _proposalId);
+        vm.expectRevert(abi.encodeWithSelector(IGovernor.GovernorNonexistentProposal.selector, _invalidProposalId));
+        governor.cancel(_invalidProposalId);
+        vm.stopPrank();
     }
 }
