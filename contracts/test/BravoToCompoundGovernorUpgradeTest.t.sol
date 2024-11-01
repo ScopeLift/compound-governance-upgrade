56bfec1126c991333dced23235582748e9fbd213
wildmolasses <changes@gmail.com>
2024-11-01 12:35:06 -0400

    New test helper orchestration
    Co-authored-by: Gary Ghayrat <garyghayrat@users.noreply.github.com>

--- contracts/test/BravoToCompoundGovernorUpgradeTest.t.sol
+++ contracts/test/BravoToCompoundGovernorUpgradeTest.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.26;
 
-import {ProposalTest} from "contracts/test/helpers/ProposalTest.sol";
+import {CompoundGovernorTest} from "contracts/test/helpers/CompoundGovernorTest.sol";
 
-abstract contract BravoToCompoundGovernorUpgradeTest is ProposalTest {
-    function test_UpgradeToCompoundGovernor() public {
+abstract contract BravoToCompoundGovernorUpgradeTest is CompoundGovernorTest {
+    function test_SuccessfulUpgradeToCompoundGovernor() public {
         _upgradeFromBravoToCompoundGovernorViaProposalVote();
         assertEq(timelock.admin(), address(governor));
     }
@@ -14,7 +14,10 @@ abstract contract BravoToCompoundGovernorUpgradeTest is ProposalTest {
         assertEq(timelock.admin(), GOVERNOR_BRAVO_DELEGATE_ADDRESS);
     }
 
-    function testFuzz_NewGovernorProposalCanBePassedAfterSuccessfulUpgrade(uint256 _proposerIndex, uint48 _newVotingDelay) public {
+    function testFuzz_NewGovernorProposalCanBePassedAfterSuccessfulUpgrade(
+        uint256 _proposerIndex,
+        uint48 _newVotingDelay
+    ) public {
         _proposerIndex = bound(_proposerIndex, 0, _majorDelegates.length - 1);
         _upgradeFromBravoToCompoundGovernorViaProposalVote();
         assertEq(timelock.admin(), address(governor));
@@ -23,8 +26,11 @@ abstract contract BravoToCompoundGovernorUpgradeTest is ProposalTest {
         assertEq(governor.votingDelay(), _newVotingDelay);
     }
 
-    function testFuzz_RevertIf_OldGovernorAttemptsQueueingAfterSuccessfulUpgrade(uint256 _proposerIndex, uint _newVotingDelay) public {
-        uint _originalVotingDelay = GOVERNOR_BRAVO.votingDelay();
+    function testFuzz_RevertIf_OldGovernorAttemptsQueueingAfterSuccessfulUpgrade(
+        uint256 _proposerIndex,
+        uint256 _newVotingDelay
+    ) public {
+        uint256 _originalVotingDelay = GOVERNOR_BRAVO.votingDelay();
         vm.assume(_newVotingDelay != _originalVotingDelay);
         _upgradeFromBravoToCompoundGovernorViaProposalVote();
         assertEq(timelock.admin(), address(governor));
@@ -35,13 +41,32 @@ abstract contract BravoToCompoundGovernorUpgradeTest is ProposalTest {
         assertEq(GOVERNOR_BRAVO.votingDelay(), _originalVotingDelay);
     }
 
-    function testFuzz_OldGovernorProposalCanBePassedAfterFailedUpgrade(uint256 _proposerIndex, uint _newVotingDelay) public {
-        uint _originalVotingDelay = GOVERNOR_BRAVO.votingDelay();
+    function testFuzz_OldGovernorProposalCanBePassedAfterFailedUpgrade(uint256 _proposerIndex, uint256 _newVotingDelay)
+        public
+    {
+        uint256 _originalVotingDelay = GOVERNOR_BRAVO.votingDelay();
         vm.assume(_newVotingDelay != _originalVotingDelay);
         _failProposalVoteForUpgradeFromBravoToCompoundGovernor();
         assertEq(timelock.admin(), GOVERNOR_BRAVO_DELEGATE_ADDRESS);
         uint256 _proposalId = _buildAndSubmitOldGovernorSetVotingDelayProposal(_proposerIndex, _newVotingDelay);
         _passQueueAndExecuteBravoProposal(_proposalId);
         assertEq(GOVERNOR_BRAVO.votingDelay(), _newVotingDelay);
+    }
+}
+
+contract PreUpgradeTest is BravoToCompoundGovernorUpgradeTest {
+    function setUp() public virtual override {
+        super.setUp();
+    }
+
+    function _useDeployedCompoundGovernor() internal pure override returns (bool) {
+        // returning false indicates that a new CompoundGovernor should be deployed before the inherited tests are run.
+        return false;
+    }
+
+    function _shouldPassAndExecuteUpgradeProposal() internal pure override returns (bool) {
+        // returning false indicates the upgrade proposal should not be passed and executed before the inherited tests
+        // are run.
+        return false;
     }
 }
