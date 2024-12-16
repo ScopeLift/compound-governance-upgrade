75e801ad90dd84a8a43535e920793f8b3d5d83bc
John Feras <jferas@ferasinfotech.com>
2024-12-16 09:28:53 -0500

    Corrected hash of GovernorSequentialProposalIdStorageLocation

--- contracts/extensions/GovernorSequentialProposalIdUpgradeable.sol
+++ contracts/extensions/GovernorSequentialProposalIdUpgradeable.sol
@@ -34,7 +34,7 @@ abstract contract GovernorSequentialProposalIdUpgradeable is GovernorUpgradeable
     // keccak256(abi.encode(uint256(keccak256("storage.GovernorSequentialProposalIdStorage")) - 1)) &
     // ~bytes32(uint256(0xff))
     bytes32 private constant GovernorSequentialProposalIdStorageLocation =
-        0xa6952339bc887ea688c6b8e8399bb953a5002ee79177d6322fca98dc89ae0b00;
+        0x357e1d0c89980520b3654c57f444238d75a15e5f41d389a090caabe54617d800;
 
     function _getGovernorSequentialProposalIdStorage()
         private
